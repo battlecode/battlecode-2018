5fab31beb1f6b8988948ca819004221062ddece1
Gina Yuan <gina.yuan@gmail.com>
2017-12-09 23:08:57 -0500

    Restructured entity code, derived more traits, made 'info' and 'state' consistent

--- battlecode-engine/src/entity.rs
+++ battlecode-engine/src/entity.rs
@@ -1,73 +1,63 @@
 //! Entities are player-controlled entities with certain characteristics and
 //! game actions, depending on their type.
 
-use super::location::MapLocation;
+use super::location;
 use super::world::Team;
-use entity::RobotType::*;
+use entity::Entity::*;
 
 /// The ID of an entity is assigned when the entity is spawned. Each entity ID
 /// is unique and in the range [0, 65,535], inclusive.
 pub type EntityID = u16;
 
-/// RobotType names the different robot types.
-#[derive(Debug, Serialize, Deserialize)]
-pub enum RobotType {
-    Knight,
+/// Info specific to knights.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct KnightInfo {
+
 }
 
-/// RobotTypeInfo contains details on various attributes of the different
-/// robots. All of this information is in the specs in a more organized form.
-#[derive(Debug, Serialize, Deserialize)]
-pub struct RobotTypeInfo {
-    pub robot_type: RobotType,
-    pub max_health: u32,
-    pub damage: u32,
-}
-
-impl RobotTypeInfo {
-    pub fn new(robot_type: RobotType) -> RobotTypeInfo {
-        match robot_type {
-            Knight => RobotTypeInfo {
-                robot_type: Knight,
-                max_health: 250,
-                damage: 100,
-            }
-        }
-    }
-}
-
-/// RobotInfo stores basic information of a robot that is public to both teams.
-/// This info is ephemeral and there is no guarantee any of it will remain the
-/// same between rounds.
-#[derive(Debug, Serialize, Deserialize)]
-pub struct RobotInfo {
-    pub id: EntityID,
-    pub team: Team,
-    pub robot_type: RobotType,
-    pub location: MapLocation,
-    pub health: u32,
-}
-
-/// RobotInfo stores basic information of a rocket that is public to both teams.
-/// This info is ephemeral and there is no guarantee any of it will remain the
-/// same between rounds.
-#[derive(Debug, Serialize, Deserialize)]
+/// Info specific to rockets.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct RocketInfo {
-    pub id: EntityID,
-    pub team: Team,
-    pub location: MapLocation,
-    pub health: u32,
+
 }
 
-/// FactoryInfo stores basic information of a factory that is public to both
-/// teams. This info is ephemeral and there is no guarantee any of it will
-/// remain the same between rounds.
-#[derive(Debug, Serialize, Deserialize)]
+/// Info specific to factories.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct FactoryInfo {
+    /// Entities queued to be produced.
+    production_queue: Vec<Entity>,
+}
+
+/// Entities are player-controlled entities with certain characteristics and
+/// game actions, depending on their type.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub enum Entity {
+    /// Knights are a melee unit that is strong in numbers.
+    Knight(KnightInfo),
+    /// Factories are the hub for producing combative robots.
+    Factory(FactoryInfo),
+    /// Rockets are the only unit that can move between planets.
+    Rocket(RocketInfo),
+}
+
+/// Generic info for a single entity, and the associated body.
+pub struct EntityInfo {
     pub id: EntityID,
     pub team: Team,
-    pub location: MapLocation,
+    pub max_health: u32,
+    pub location: location::MapLocation,
     pub health: u32,
+
+    /// The associated body (a robot, factory, or rocket).
+    pub body: Entity,
+}
+
+/// Moves a robot to the given location.
+pub fn move_location(entity: &EntityInfo, location: &location::MapLocation) {
+    match &entity.body {
+        &Knight(ref knight) => (),
+        _ => ()
+    }
 }
 
 #[cfg(test)]
