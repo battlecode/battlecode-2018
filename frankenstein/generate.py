8612ad26dce2e31d68e46c119a214fb7f7badd49
James Gilles <jhgilles@mit.edu>
2017-12-14 14:24:15 -0500

    Constructors, docs

--- frankenstein/generate.py
+++ frankenstein/generate.py
@@ -34,6 +34,7 @@ u32 = Type('u32', 'uint32_t')
 i32 = Type('i32', 'int32_t')
 u64 = Type('u64', 'uint64_t')
 i64 = Type('i64', 'int64_t')
+void = Type('()', 'void')
 
 class Var(object):
     def __init__(self, type, name):
@@ -72,25 +73,52 @@ class Function(object):
         return result
 
 class Struct(object):
-    def __init__(self, name, opaque=False):
+    def __init__(self, name, opaque=False, docs=''):
         self.name = name
         self.members = []
+        self.member_docs = []
         self.methods = []
         self.method_names = []
+        self.method_docs = []
         self.getters = []
         self.type = Type(name, name)
         self.opaque = opaque
-    
-    def member(self, type, name):
+        self.constructor_ = None
+        self.constructor_docs = ''
+        self.destructor = Function(void, 'delete_'+self.name, [Var(self.type.mut_ptr(), 'self')],
+            '\tBox::from_raw(self)'
+        )
+        self.docs = docs
+
+    def self_pointer(self):
+        return Var(self.type.mut_ptr(), 'self')
+
+    def constructor(self, rust_method, args, docs=''):
+        assert self.constructor_ is None
+        self.constructor_docs = docs
+
+        constructor_call = '{}::{}'.format(self.name, rust_method)
+        constructor_call += '(' + ', '.join(a.name for a in args) + ')'
+
+        self.constructor_ = Function(
+            self.type.mut_ptr(),
+            'new_' + self.name,
+            args,
+            '\tBox::into_raw(Box::new(check({})))'.format(constructor_call)
+        )
+
+        return self
+
+    def member(self, type, name, docs=''):
         self.members.append(Var(type,name))
+        self.member_docs.append(docs)
         if self.opaque:
-            mut_self = self.type.mut_ptr()
             # getter and setter functions to access non-repr(C) structs
-            getter = Function(type, self.name + "_get_" + name, [Var(mut_self, 'self')],
-                '\t(*self).{0}.clone()'.format(name)
+            getter = Function(type, self.name + "_get_" + name, [self.self_pointer()],
+                '\tcheck((*self).{0}.clone())'.format(name)
             )
-            setter = Function(type, self.name + "_set_" + name,
-                [Var(mut_self,'self'), Var(type,name)],
+            setter = Function(void, self.name + "_set_" + name,
+                [self.self_pointer(), Var(type,name)],
                 '\t(*self).{0} = {0};'.format(name) 
             )
             self.getters.append(getter)
@@ -98,16 +126,15 @@ class Struct(object):
 
         return self
 
-    def self_pointer(self):
-        return Var(self.type.mut_ptr(), 'self')
 
-    def method(self, type, name, args):
+    def method(self, type, name, args, docs=''):
         self.methods.append(Function(type, self.name + '_' + name, [self.self_pointer()] + args,
-            '\t(&mut (*self)).{}('.format(name) +
+            '\tcheck((&mut (*self)).{}('.format(name) +
             ', '.join('{0.name}'.format(arg) for arg in args[1:]) +
-            ')'
+            '))'
         ))
         self.method_names.append(name)
+        self.method_docs.append(docs)
         return self
 
     def _c_struct(self):
@@ -121,14 +148,22 @@ class Struct(object):
         return definition
 
     def to_c(self):
+        assert self.constructor_ is not None
         definition = self._c_struct()
-        methods = ''.join(method.to_swig() for method in self.methods)
-        getters = ''.join(getter.to_swig() for getter in self.getters)
-        return definition + methods + getters
+        definition += self.constructor_.to_c()
+        definition += self.destructor.to_c()
+        definition += ''.join(getter.to_c() for getter in self.getters)
+        definition += ''.join(method.to_c() for method in self.methods)
+        return definition
 
     def to_swig(self):
         '''Generate a SWIG interface for this struct.'''
-        definition = self._c_struct()
+        assert self.constructor_ is not None
+        definition = '%feature("docstring", "{}");\n'.format(self.docs)
+        if self.opaque:
+            definition += 'typedef struct {{}} {0.name};\n'.format(self)
+        else:
+            definition += self._c_struct()
         # We use SWIG's %extend command to attach "methods" to this struct:
         # %extend Bananas {
         #     int peel(int);
@@ -138,12 +173,16 @@ class Struct(object):
         # int Bananas_peel(Bananas *self, int)
         # which we generate :)
         extra = '%extend {0.name} {{\n'.format(self)
-        for method, method_name in zip(self.methods, self.method_names):
+        extra += ''
+
+        for method, method_name, method_docs in zip(self.methods, self.method_names, self.method_docs):
+            extra += '\t%feature("docstring", "{}");\n'.format(method_docs)
             extra += '\t' + Function(method.type, method_name, method.args[1:]).to_swig()
         if self.opaque:
-            for member in self.members:
+            for member, member_docs in zip(self.members, self.member_docs):
+                extra += '\n\t%feature("docstring", "{}");\n'.format(member_docs)
                 # add getters
-                extra += '\n\t' + member.to_swig() + ';'
+                extra += '\t' + member.to_swig() + ';'
 
         extra += '\n};'
 
@@ -151,18 +190,22 @@ class Struct(object):
 
     def to_rust(self):
         '''Generate a rust implementation for this struct.'''
+        assert self.constructor_ is not None
         # assume that struct is already defined
-        result = ''.join(method.to_rust() for method in self.methods)
-        result += ''.join(getter.to_rust() for getter in self.getters)
+        definition = self.constructor_.to_rust()
+        definition += self.destructor.to_rust()
+        definition += ''.join(getter.to_rust() for getter in self.getters)
+        definition += ''.join(method.to_rust() for method in self.methods)
 
-        return result
+        return definition
 
 SWIG_HEADER = '''%module {module}
 /// GENERATED SWIG
+%feature("autodoc", "1");
 #include <stdint.h>
 %{{
 #include "{module}.h"
-}}
+%}}
 '''
 
 RUST_HEADER = '''/// GENERATED RUST
@@ -203,10 +246,11 @@ class Program(object):
 
 p = Program('speleothem')\
     .add(Function(u8, 'ziggurat', [Var(u8, 'taekwondo'), Var(u32, 'explosion')]))\
-    .add(Struct('Banana', opaque=True)\
+    .add(Struct('Banana', opaque=True, docs='High in potassium.')\
+        .constructor('new', [])\
         .member(u8, 'hello')\
         .member(u32, 'goodbye')\
-        .method(i64, 'peel', [Var(i16, 'amount')])\
+        .method(i64, 'peel', [Var(i16, 'amount')], docs='be careful!!')\
         .method(i64, 'consume', [Var(u8, 'greedy'), Var(i16, 'solipsistic')])\
         .method(i64, 'peel_harder', [Var(i16, 'amount'), Var(u64, 'veracity')])
     )
