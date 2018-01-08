f99fc15e105e3c7e95cce11d88096eb1d0ce0924
James Gilles <jhgilles@mit.edu>
2018-01-08 10:51:59 -0500

    Travis

--- bindings/java/tests/Test.java
+++ bindings/java/tests/Test.java
@@ -1,10 +1,14 @@
 import bc.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 class Test {
     public static void main(String[] args) {
         System.out.println("'"+System.getProperty("java.library.path")+"'");
         //System.loadLibrary("libbattlecode.so");
-        System.load("/Users/james/Dev/battlecode-2018/bindings/java/src/bc/libbattlecode.so");
+        String p = Paths.get("./src/bc/libbattlecode.so").toAbsolutePath().toString();
+        System.out.println(p);
+        System.load(p);
         System.out.println(Direction.North);
         System.out.println(new MapLocation(Planet.Earth, 0, 1));
         System.out.println(new MapLocation(Planet.Earth, 0, 1).equals(new MapLocation(Planet.Earth, 0, 1)));
