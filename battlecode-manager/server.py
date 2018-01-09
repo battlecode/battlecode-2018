72ef6d52489e430afbbb22e1cb85252e2f963f5b
Joshua Segaran <jsegaran1@gmail.com>
2018-01-09 10:38:08 -0500

    Increased blocking time for unixstream

--- battlecode-manager/server.py
+++ battlecode-manager/server.py
@@ -348,7 +348,7 @@ def create_receive_handler(game: Game, dockers, use_docker: bool,
             self.logged_in = False
             logging.debug("Client connected to server")
             # TODO check if this is enough time out is generous enough
-            self.request.settimeout(5)
+            self.request.settimeout(50)
 
             # Handle Login phase
             while not self.logged_in:
@@ -446,7 +446,6 @@ def create_receive_handler(game: Game, dockers, use_docker: bool,
                     self.docker.pause()
                 """
                 self.game.end_turn()
-                print("exiting")
 
         def viewer_handler(self):
             '''
