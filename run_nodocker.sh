9175f8ef4df609eeaac25521d59fd05ccbdd763e
James Gilles <jhgilles@mit.edu>
2018-01-11 12:05:16 -0500

    Cygwin check 4

--- run_nodocker.sh
+++ run_nodocker.sh
@@ -5,11 +5,11 @@ mtput() {
     fi
 }
 if uname -s | grep -Fqe CYGWIN ; then
-    echo "run_nodocker.sh won't work on windows! Use run.bat :)"
+    echo "run_nodocker.sh won't work on windows! Use run_nodocker.bat :)"
     exit 1
 fi
 if uname -s | grep -Fqe MINGW ; then
-    echo "run_nodocker.sh won't work on windows! Use run.bat :)"
+    echo "run_nodocker.sh won't work on windows! Use run_nodocker.bat :)"
     exit 1
 fi
 
