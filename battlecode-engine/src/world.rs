2fbf197461b9823bdd6b837ebe2f295e97888897
Gina Yuan <gina.yuan@gmail.com>
2018-01-15 16:16:06 -0500

    Fix snipe tests

--- battlecode-engine/src/world.rs
+++ battlecode-engine/src/world.rs
@@ -2757,7 +2757,7 @@ mod tests {
         // Enough rounds pass where Ranger's snipe is processed
         let rounds = 200;
         for _ in 0..4*rounds {
-            world.end_turn();
+            world.end_turn(FILLER_TIME);
         }
         
         // Robot at sniped location should take damage
@@ -3492,8 +3492,8 @@ mod tests {
         let mars_enemy = world.create_unit(Team::Blue, mars_loc.add(Direction::East), UnitType::Healer).unwrap();
 
         // go to red mars turn
-        world.end_turn();
-        world.end_turn();
+        world.end_turn(FILLER_TIME);
+        world.end_turn(FILLER_TIME);
 
         // sense that unit
         let player = Player::new(Team::Red, Planet::Mars);
