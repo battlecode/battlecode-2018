c83d55f2a22c2cb06f1c47bef8624915fbc45808
joshuagruenstein <joshuagruenstein@gmail.com>
2018-01-16 14:49:12 -0500

    remove POST/GET

--- battlecode-manager/scrimmage.py
+++ battlecode-manager/scrimmage.py
@@ -1,4 +1,3 @@
-from werkzeug.wrappers import Request, Response
 import psycopg2
 import json
 import os
@@ -11,29 +10,6 @@ import time
 import nonsense
 import random
 import proxyuploader
-
-##### SCRIMMAGE SERVER 2K18 #######
-##
-##     We put a JSON post request to the scrimmage server.
-##        {"password":"secret","red_key":"redawskey","blue_key":"blueawskey","map":"string","mapfile":{json of mapfile}}
-##        Pass either map or mapfile, but not both
-##
-##     The scrimmage server returns the JSON {'game_id':random_game_id} to the POST request
-##
-##     If a game is currently not running, the scrimmage server creates an entry in the DB with the ID, red_key, blue_key,
-##     map, mapfile, and current time, with a status field set to 1 (running).  The game is started.
-##
-##     If a game is currently running, an entry is once again created, but this time with status set to 0 (queued).
-##
-##     When the match is over, the scrimmage server sets that DB row status to 2 if player 1 wins, or 3 if player 2 wins,
-##     and uploads the matchfile to s3.  The DB entry is updated with the matchfile key and logs for red/blue_earth/mars.
-##
-##     When not running matches, the scrimmage server polls the database for matches with status 1 started greater than N
-##     seconds ago, or any matches with status 0 (queued), and runs them, resetting the start time.
-##     N is the maximum time to run a match * 1.5.
-##
-##     At any time we can GET a status JSON {'games_run':[listofidseverrun],'busy':true/false}
-##
 
 pg = None
 cur = None
@@ -159,38 +135,7 @@ def poll_thread():
 
         DB_LOCK = False
 
-@Request.application
-def application(request):
-    global DB_LOCK
-    if request.method == 'GET':
-        return Response(json.dumps({'games_run':GAMES_RUN,'busy':BUSY}))
-    elif request.method == 'POST':
-        data = json.loads(request.data)
-
-        if not 'password' in data:
-            return Response(json.dumps({'error':'No password provided.'}),401)
-        elif data['password'] != os.environ['PASSWORD']:
-            return Response(json.dumps({'error':'Incorrect password.'}),401)
-
-        if not ('red_key' in data and 'blue_key' in data and 'map' in data):
-            return Response(json.dumps({'error':'Not all fields provided.'}),400)
-
-        while DB_LOCK == True:
-            sleep(0.1)
-        DB_LOCK = True
-        cur.execute("INSERT INTO " + os.environ["TABLE_NAME"] + " (red_key, blue_key, map, status, start) VALUES (%s, %s, %s," + ('queued' if BUSY else 'running') + ", now()) RETURNING id", (data['red_key'],data['blue_key'],data['map']))
-
-        pg.commit()
-        game_id = cur.fetchone()[0]
-        DB_LOCK = False
-        if not BUSY:
-            data['id'] = game_id
-            run_match(data)
-
-        return Response(json.dumps({'game_id':game_id}))
-
 if __name__ == "__main__":
-    from werkzeug.serving import run_simple
     try:
         pg = psycopg2.connect("dbname='battlecode' user='battlecode' host='" + os.environ["DB_HOST"] + "' password='" + os.environ["DB_PASS"] + "'")
         cur = pg.cursor()
@@ -198,5 +143,4 @@ if __name__ == "__main__":
     except:
         print("Could not connect to postgres.")
 
-    threading.Thread(target=poll_thread).start()
-    run_simple('0.0.0.0', 410, application, use_reloader=True)
+    poll_thread()
