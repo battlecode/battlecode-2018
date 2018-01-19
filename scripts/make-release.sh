2f4830590092c94f840f7072f06382f824bf5687
James Gilles <jhgilles@mit.edu>
2018-01-19 18:09:37 -0500

    Release 0.13.0

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -56,7 +56,7 @@ prompt() {
     done
 }
 
-RELEASE=0.12.2
+RELEASE=0.13.0
 
 green
 echo "=== Starting release $(magenta)$RELEASE$(green) ==="
@@ -92,7 +92,7 @@ if [ -z "$(git status --porcelain | grep -v make-release.sh)" ]; then
 fi
 
 step cd bc18-scaffold
-if git fetch origin && git checkout $RELEASE; then
+if git fetch origin && git pull origin && git checkout $RELEASE; then
     true
 else
     red
@@ -102,41 +102,41 @@ else
 fi
 step cd ..
 
-#step make clean
-#step make release
-#step mv battlecode battlecode-mac
-#step make clean
-#step make linux-libs
-#step make clean
-#step make docker-sandbox
-#step make docker-manager
-#
-#step mv battlecode-mac battlecode
-#step cp -R docker-artifacts/battlecode-linux/* battlecode/
-#step ls battlecode/c/lib
-#step ls battlecode/java/bc/
-#step ls battlecode/python/battlecode/darwin
-#step ls battlecode/python/battlecode/linux
-#
-#prompt "files look good?"
-#
-#blue
-#echo "Please wait for the following matches to finish."
-#plain
-#step ./battlecode.sh -p1 examplefuncsplayer-python -p2 examplefuncsplayer-java -m bananas
-#step ./battlecode.sh -p1 examplefuncsplayer-c -p2 examplefuncsplayer-c -m bananas
-#echo
-#blue
-#echo "Please run matches between examplefuncsplayer-python, examplefuncsplayer-java, examplefuncsplayer-c, then terminate the manager with Stop Manager."
-#plain
-#step_ignore ./run_nodocker.sh
-#prompt "Did it work?"
-#
-#blue
-#echo "Please run matches between examplefuncsplayer-python, examplefuncsplayer-python-old, examplefuncsplayer-java, examplefuncsplayer-java-old, examplefuncsplayer-c, examplefuncsplayer-c-old, then terminate the manager with Stop Manager."
-#plain
-#step_ignore docker run -it --privileged -p 16147:16147 -p 6147:6147 -v $PWD:/player --rm battledaddy
-#prompt "Did it work?"
+step make clean
+step make release
+step mv battlecode battlecode-mac
+step make clean
+step make linux-libs
+step make clean
+step make docker-sandbox
+step make docker-manager
+
+step mv battlecode-mac battlecode
+step cp -R docker-artifacts/battlecode-linux/* battlecode/
+step ls battlecode/c/lib
+step ls battlecode/java/bc/
+step ls battlecode/python/battlecode/darwin
+step ls battlecode/python/battlecode/linux
+
+prompt "files look good?"
+
+blue
+echo "Please wait for the following matches to finish."
+plain
+step ./battlecode.sh -p1 examplefuncsplayer-python -p2 examplefuncsplayer-java -m bananas
+step ./battlecode.sh -p1 examplefuncsplayer-c -p2 examplefuncsplayer-c -m bananas
+echo
+blue
+echo "Please run matches between examplefuncsplayer-python, examplefuncsplayer-java, examplefuncsplayer-c, then terminate the manager with Stop Manager."
+plain
+step_ignore ./run_nodocker.sh
+prompt "Did it work?"
+
+blue
+echo "Please run matches between examplefuncsplayer-python, examplefuncsplayer-python-old, examplefuncsplayer-java, examplefuncsplayer-java-old, examplefuncsplayer-c, examplefuncsplayer-c-old, then terminate the manager with Stop Manager."
+plain
+step_ignore docker run -it --privileged -p 16147:16147 -p 6147:6147 -v $PWD:/player --rm battledaddy
+prompt "Did it work?"
 
 step git add battlecode-manager/web/run.html
 step git add .
